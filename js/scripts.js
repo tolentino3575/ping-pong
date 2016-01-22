0da423fd7d303c2ef648033963a5eb0630ae3728
erik <student@epicodus.com>
2016-01-22 10:00:25 -0800

    Add functionality to third test, pass 2nd test, rearrange order to pass 3rd test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var pizza = function(input) {
-    if (input % 3 === 0) {
-      return "pizza";
+    if (input % 15 === 0) {
+      return "pizzapie";
+  } else if (input % 3 === 0) {
+    return "pizza";
   } else if (input % 5 === 0) {
     return "pie";
   }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,9 @@
 describe('pizza', function() {
 
+  it("is a number divisible by 15, change to pizzapie", function() {
+    expect(pizza(15)).to.equal("pizzapie");
+  });
+
   it("is a number divisible by 3, change to pizza", function() {
     expect(pizza(3)).to.equal("pizza");
   });
@@ -7,6 +11,7 @@ describe('pizza', function() {
   it("is a number divisible by 5, change to pie", function() {
     expect(pizza(5)).to.equal("pie");
   });
+
 
 
 
