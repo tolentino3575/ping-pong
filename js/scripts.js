f9fa9d040a9716b1acd20527d00811b80bef0384
erik <student@epicodus.com>
2016-01-22 09:16:03 -0800

    add divisible by 3 functionality

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var pizzaPie = function(pizza) {
+  return true;
+}
+
+// $(document).ready(function(){
+//   $("form#pizzaPie").submit(function(event) {
+//
+//
+//     var inputNum = parseInt($("input#input").val());
+//     for (i = 0, i < inputNum, i++) {
+//       if (inputNum % 3 === 0) {
+//         return true;
+//       }
+//     }
+//
+//
+//
+//
+//   event.preventDefault();
+//   });
+// });
---
+++ specs/specs.js
@@ -0,0 +1,14 @@
+describe("pizzaPie", function() {
+  it("is a number divisible by 3", function() {
+    expect(pizzaPie("number")).to.equal(true);
+  });
+
+
+
+
+
+
+
+
+
+});
