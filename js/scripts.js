c114fd2dbf740ebad7ae0f03869ad716335b1102
erik <student@epicodus.com>
2016-01-22 15:41:44 -0800

    Make functionality work in browser

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,68 @@
-var pizza = function(input) {
-    if (input % 15 === 0) {
-      return "pizzapie";
-  } else if (input % 3 === 0) {
-    return "pizza";
-  } else if (input % 5 === 0) {
-    return "pie";
-  } else {
-    return true;
-  }
+// var numList = function(inputNum) {
+//   var array = []
+//   for (var i =1; 1 <= inputNum; i++) {
+// //     array.push(i);
+//   }
+//   return array
+// }
+
+var numReplace = function(inputNum) {
+    var array = []
+    for (var i = 1; i <= inputNum; i++) {
+      if (i % 15 === 0) {
+        array.push("pizza-pie!");
+    } else if (i % 3 === 0) {
+        array.push("pizza!");
+    } else if (i % 5 === 0) {
+        array.push("pie!");
+    } else {
+        array.push(i);
+    }
+  };
+  return array;
 };
 
 
 
+$(document).ready(function(){
+  $("form#pizzaPie").submit(function(event) {
+    var inputNum = parseInt($("input#userInput").val());
+
+    var resultArray = numReplace(inputNum)
+    var result = resultArray.toString();
+
+    $("#result").show();
+    $("#resultNums").append(result);
+
+  event.preventDefault();
+  });
+});
 
 
+// var numReplace = function(inputNum) {
+//       if (inputNum % 15 === 0) {
+//         return ("pizza-pie!");
+//     } else if (inputNum % 3 === 0) {
+//         return ("pizza!");
+//     } else if (inputNum % 5 === 0) {
+//         return ("pie!");
+//     } else {
+//         return ("i");
+//     }
+//   };
 
-// for (i = 0, i < inputNum, i++) {
-//    if (inputNum % 3 === 0) {
-//      return pizza;
-//    } else if (inputNum % 5 === 0) {
-//      return pie;
-//    } else if (inputNum % 15 === 0) {
-//      return pizzapie;
-//    } else {
-//      return inputNum;
-//    }
-//  }
 
-
-//   $(document).ready(function(){
-//     $("form#pizzaPie").submit(function(event)) {
-//       var inputNum = parseInt($("input#input").val());
-//
-//
-//       event.preventDefault();
-//   });
-// });
+// var numReplace = function(inputNum) {
+//     var array = []
+//     for (i = 1, i <= inputNum, i++);
+//       if (inputNum % 15 === 0) {
+//         array.push("pizza-pie!");
+//     } else if (inputNum % 3 === 0) {
+//         array.push("pizza!");
+//     } else if (inputNum % 5 === 0) {
+//         array.push("pie!");
+//     } else {
+//         array.push(inputNum);
+//     }
+//     return array;
+//   };
--- specs/specs.js
+++ specs/specs.js
@@ -1,27 +1,19 @@
-describe('pizza', function() {
+describe('numReplace', function() {
 
-  it("is a number divisible by 15, change to pizzapie", function() {
-    expect(pizza(15)).to.equal("pizzapie");
+  it("is a number divisible by 3, change to pizza!", function() {
+    expect(numReplace(3)).to.equal("pizza!");
   });
 
-  it("is a number divisible by 3, change to pizza", function() {
-    expect(pizza(3)).to.equal("pizza");
+  it("is a number divisible by 5, change to pie!", function() {
+    expect(numReplace(5)).to.equal("pie!");
   });
 
-  it("is a number divisible by 5, change to pie", function() {
-    expect(pizza(5)).to.equal("pie");
+  it("is a number divisible by 15, change to pizza-pie!", function() {
+    expect(numReplace(15)).to.equal("pizza-pie!");
   });
 
   it("is a number not divisible by 3, 5, 15, return number", function() {
-    expect(pizza(7)).to.equal(true);
+    expect(numReplace(7)).to.equal("i");
   });
-
-
-
-
-
-
-
-
 
 });
